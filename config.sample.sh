5f7080b58ffabf1e2d12896117552372600f0956
Corey Blaz <corey@blazdesign.com>
2016-05-15 09:24:53 -0700

    Added sample config file.

---
+++ config.sample.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# WordPress installation directory
+DIR="your.wordpressinstall.com"
+
+# Dropbox folder to put files
+DROPBOX_DIR="wordpress-install-backup"
+
+# Email address to report to
+EMAIL_ADDRESS="your@emailaddress.com"
