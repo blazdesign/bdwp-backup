0fb90a5ab09ce578912b7fc04278ec16fc3f1d2d
blazdesign <corey@blazdesign.com>
2017-08-06 12:08:01 -0400

    Put the config file back in because removing it was a stupid idea and makes doing a git pull a pain.

---
+++ sample.config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#############################################
+#             Config
+#############################################
+
+# Absolotue WordPress installation directory;
+# this is necessary for crontab
+WP_DIR="/home/user/example.com"
+
+# Backup name
+DIR="example.com"
+
+# FTP folder to put files
+FTP_DIR="example"
+
+# Email address to report to
+EMAIL_ADDRESS=""
+# Email report?
+EMAIL_REPORT="FALSE"
+
+# FTP  Info
+FTP_SERVER=""
+FTP_USERNAME=""
+FTP_PASSWORD=""
+
+#############################################
+
